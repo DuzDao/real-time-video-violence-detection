e3c8690873ba14ad68b656690d165b64493f9867
DuzDao <duzdao.workspace@gmail.com>
2024-06-27 05:24:06 +0700

    First commit

---
+++ kafka_services/video_producer.py
@@ -0,0 +1,63 @@
+import cv2
+import time
+import numpy as np
+from skimage.transform import resize
+
+from kafka import KafkaProducer
+
+class VideoProducer:
+    def __init__(self, config):
+        self.bootstrap_servers = config["bootstrap_servers"]
+        self.topic = config["topic"]
+        self.interval = config["interval"]
+        self.producer = KafkaProducer(bootstrap_servers = self.bootstrap_servers)
+
+
+    def produce(self, frame):
+        """
+        Encode & produce frame.
+        ----------
+        `frame`: numpy.ndarray | [A video frame]
+        """
+        frame = self.process_frame(frame)
+
+        _, buffer = cv2.imencode('.jpg', frame)
+
+        self.producer.send(self.TOPIC, buffer.tobytes())
+
+        time.sleep(self.interval)
+
+
+    def publish_from_video(self, mp4_path):
+        """
+        Read video frame, process frame & produce.
+        ----------
+        `mp4_path`: str     | [Path of mp4 video]
+        """
+        video = cv2.VideoCapture(mp4_path)
+        
+        while video.isOpened():
+            success, frame = video.read()
+
+            if not success:
+                print("Read video not success!")
+                break
+            
+            self.produce(frame)
+        video.release()
+
+        
+    def process_frame(self, frame):
+        """
+        Resize video frame to (1, 160, 160, 3)
+        ----------
+        `frame`: numpy.ndarray | [A video frame]
+        """
+
+        # resize
+        frame = resize(frame, (160,160,3))  
+        frame = np.expand_dims(frame, axis=0)
+
+        # scale
+        frame = frame / 255.0
+        return frame            
---
+++ producer.py
@@ -0,0 +1,18 @@
+import yaml
+import argparse
+from kafka_services.video_producer import VideoProducer
+
+def main(args):
+    with open(args.config) as conf_file:
+        config = yaml.safe_load(conf_file)
+    
+    producer = VideoProducer(config)
+    producer.publish_from_video(args.video_path)
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args_parser.add_argument('--video', dest='video_path', required=True)
+
+    args = args_parser.parse_args()
+    main(args)
